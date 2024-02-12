565bdde32c4065b1f871e2fe571a5121e2c635c4
hassaanhabib23 <128929040+hassaanhabib23@users.noreply.github.com>
2024-02-12 22:37:30 +0500

    Basic Structure

---
+++ gpa_calculator.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="gpa_calculator.css">
+    <title>GPA Calculator</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <h3>GPA Calculator</h3>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>Course Name</h4>
+                    </td>
+                    <td class="Marksfieldheading">
+                        <h4>Obtained Assignment Marks</h4>
+                    </td>
+                    <td class="Marksfieldheading">
+                        <h4>Obtained Quiz Marks</h4>
+                    </td>
+                    <td class="Marksfieldheading">
+                        <h4>Mid Marks</h4>
+                    </td>
+                    <td class="Marksfieldheading">
+                        <h4>Final Marks</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td><input type="text" placeholder="Course 1" class="coursefieldStyling"></td>
+                    <td><input type="number" min="0" max="10" placeholder="Total 10" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="10" placeholder="Total 10" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="25" placeholder="Total 25" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="50" placeholder="Total 50" class="marksfieldStyling"></td>
+                </tr>
+                <tr>
+                    <td> <input type="text" placeholder="Course 2" class="coursefieldStyling"></td>
+                    <td><input type="number" min="0" max="10" placeholder="Total 10" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="10" placeholder="Total 10" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="25" placeholder="Total 25" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="50" placeholder="Total 50" class="marksfieldStyling"></td>
+                </tr>
+                <tr>
+                    <td><input type="text" placeholder="Course 3" class="coursefieldStyling"></td>
+                    <td><input type="number" min="0" max="10" placeholder="Total 10" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="10" placeholder="Total 10" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="25" placeholder="Total 25" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="50" placeholder="Total 50" class="marksfieldStyling"></td>
+                </tr>
+                <tr>
+                    <td> <input type="text" placeholder="Course 4" class="coursefieldStyling"></td>
+                    <td><input type="number" min="0" max="10" placeholder="Total 10" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="10" placeholder="Total 10" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="25" placeholder="Total 25" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="50" placeholder="Total 50" class="marksfieldStyling"></td>
+                </tr>
+                <tr>
+
+                    <td><input type="text" placeholder="Course 5" class="coursefieldStyling"></td>
+                    <td><input type="number" min="0" max="10" placeholder="Total 10" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="10" placeholder="Total 10" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="25" placeholder="Total 25" class="marksfieldStyling"></td>
+                    <td><input type="number" min="0" max="50" placeholder="Total 50" class="marksfieldStyling"></td>
+                </tr>
+                <tr>
+                    <td><button>Calculate</button></td>
+                </tr>
+                <tr>
+                    <td><label> </label></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+
+</html>
